75147c9633b1ed73610929d8956fedcbbf126b71
Kim-Ha-Jeong <kha0318@naver.com>
2020-02-02 00:31:18 +0900

    전체 css 정리

--- Recruiting2020/client/src/component/result/Find.js
+++ Recruiting2020/client/src/component/result/Find.js
@@ -1,19 +1,19 @@
 
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Row } from 'antd';
+import { Row, Form, message } from 'antd';
 import './Result.css';
 
-export default class Find extends Component {
+class Find extends Component {
   constructor(props) {
     super(props);
-    
+
     this.onChangeName = this.onChangeName.bind(this);
     this.onChangeStudent_id = this.onChangeStudent_id.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-      result:'',
+      result: '',
       name: '',
       student_id: '',
       key: ''
@@ -28,56 +28,76 @@ export default class Find extends Component {
   onChangeStudent_id(e) {
     this.setState({
       student_id: e.target.value
-    })  
+    })
   }
-  
+
   onSubmit(e) {
     e.preventDefault();
-    axios.post('http://localhost:4000/result/find/'+this.state.student_id+"/"+this.state.name)
-        .then((res) => this.setState({
-          result: res.data
-        }))
-        .then(res => this.state.result.map((x,i) => this.setState({
-          key : x.key
-        })))
-        .then(res => this.props.history.push('/result/'+this.state.key));
+    this.props.form.validateFields((err, values) =>{
+      if(!err){
+        axios.post('http://localhost:4000/result/find/' + this.state.student_id + "/" + this.state.name)
+      .then((res) => this.setState({
+        result: res.data
+      }))
+      .then(res => this.state.result.map((x, i) => this.setState({
+        key: x.key
+      })))
+      .then(res => this.props.history.push('/result/' + this.state.key));
+      } else {
+        message.error("성명,학번 둘 다 정확히 입력했는지 확인해주세요!")
+      }
+    })
+    
   }
 
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-        <div id="wrapper">
-          <Row>
-            <h2 id="title" align="center">결과 확인</h2>
-          </Row>
-            <hr/>
-          <div className="main-container">
-            <form onSubmit={this.onSubmit}>
+      <div id="wrapper">
+        <Row>
+          <h2 id="title" align="center">결과 확인</h2>
+        </Row>
+        <hr />
+        <div className="find-container">
+          <Form onSubmit={this.onSubmit}>
+            <Form.Item>
+              {getFieldDecorator('name', {
+                rules: [{ required: true, message: '성명을 입력해주세요!' }],
+              })(
                 <div className="form-group">
-                    <label>성명</label>
-                    <input 
-                      type="text" 
-                      className="form-control"
-                      onChange={this.onChangeName}
-                      />
-                </div>
+                  <label>성명</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={this.onChangeName}
+                  />
+                </div>)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('student_id', {
+                rules: [{ required: true, message: '학번을 입력해주세요!' }],
+              })(
                 <div className="form-group">
-                    <label>학번</label>
-                    <input type="text" 
-                      className="form-control"
-                      onChange={this.onChangeStudent_id}
-                      />
-                </div>
-                <div className="form-group" align="center">
-                     <input type="submit"
-                      value="확인"
-                      className="ant-btn ant-btn-round"
-                      id="submit-btn"
-                     />
-                </div>
-            </form>
-          </div>
+                  <label>학번</label>
+                  <input type="text"
+                    className="form-control"
+                    onChange={this.onChangeStudent_id}
+                  />
+                </div>)}
+            </Form.Item>
+            <div className="form-group" align="center">
+              <input type="submit"
+                value="확인"
+                className="ant-btn ant-btn-round"
+                id="submit-btn"
+              />
+            </div>
+          </Form>
+        </div>
       </div>
 
     )
   }
 }
+
+export default Form.create()(Find);
