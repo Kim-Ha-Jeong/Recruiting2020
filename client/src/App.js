a90ff13e9257af64195aa6c32a9e5daed15b0e05
chaezer0 <pcy07111@naver.com>
2020-02-09 23:46:10 +0900

    about page outline

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import About from './component/About';
-import Home from './component/Home';
-import NoticeManager from './component/notice/NoticeManager';
-import Submit from './component/Submit';
-import Header from './component/Header';
-import Notice from './component/notice/Notice';
-import Edit from './component/notice/Edit'
-import Create from './component/notice/Create';
-import Find from './component/result/Find';
-import Result from './component/result/Result';
-import Undefine from './component/result/Undefine';
-import Research1 from './component/research/research1';
-import Research2 from './component/research/research2';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import About from "./component/About";
+import Home from "./component/Home";
+import NoticeManager from "./component/notice/NoticeManager";
+import Submit from "./component/Submit";
+import Header from "./component/Header";
+import Notice from "./component/notice/Notice";
+import Edit from "./component/notice/Edit";
+import Create from "./component/notice/Create";
+import Find from "./component/result/Find";
+import Result from "./component/result/Result";
+import Undefine from "./component/result/Undefine";
+import Research1 from "./component/research/research1";
+import Research2 from "./component/research/research2";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div>
         <Header></Header>
         <div>
-          <About></About>
-          <Route path="/" component={Home} exact={true} />
+          <Route path="/" component={About} exact={true} />
           <Route path="/notice" component={Notice} />
           <Route path="/submit" component={Submit} />
           <Route path="/FAQ" component={Notice} />
-          <Route path='/noticeManager/edit/:id' component={Edit} />
+          <Route path="/noticeManager/edit/:id" component={Edit} />
           <Route path="/noticeManager" component={NoticeManager} exact={true} />
           <Route path="/noticeManager/create" component={Create} />
           <Route path="/result" component={Find} exact={true} />
@@ -35,7 +34,7 @@ class App extends Component{
           <Route path="/research2" component={Research2} />
         </div>
       </div>
-    )
+    );
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
