4ce01e298d50eb1915397a6a73ab39a22a17c114
chaezer0 <pcy07111@naver.com>
2020-02-10 19:55:29 +0900

    index.js 수정

--- server/index.js
+++ server/index.js
@@ -1,39 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const PORT = 80;
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./DB.js');
-const noticeRoute = require('./notice.route');
-const FAQRoute = require('./FAQ.route');
-const resultRoute = require('./result.route')
-const uploadRoute = require('./fileupload.route');
-const researchRoute = require('./research.route');
-const researchRoute2 = require('./research.route2');
+const bodyParser = require("body-parser");
+const PORT = 4000;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./DB.js");
+const noticeRoute = require("./notice.route");
+const FAQRoute = require("./FAQ.route");
+const resultRoute = require("./result.route");
+const uploadRoute = require("./fileupload.route");
+const researchRoute = require("./research.route");
+const researchRoute2 = require("./research.route2");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
+  () => {
+    console.log("Database is connected");
+  },
+  err => {
+    console.log("Can not connect to the database" + err);
+  }
 );
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/notice', noticeRoute);
-app.use('/FAQ', FAQRoute);
-app.use('/result', resultRoute);
-app.use('/upload', uploadRoute);
-app.use('/research', researchRoute);
-app.use('/research2', researchRoute2);
+app.use("/notice", noticeRoute);
+app.use("/FAQ", FAQRoute);
+app.use("/result", resultRoute);
+app.use("/upload", uploadRoute);
+app.use("/research", researchRoute);
+app.use("/research2", researchRoute2);
 
-app.use('/download',express.static('download'));
-app.use('/upload',express.static('uploads'));
+app.use("/download", express.static("download"));
+app.use("/upload", express.static("uploads"));
 
 app.use(cors());
 
-app.listen(PORT, function(){
-  console.log('Server is running on Port:',PORT);
+app.listen(PORT, function() {
+  console.log("Server is running on Port:", PORT);
 });
-
