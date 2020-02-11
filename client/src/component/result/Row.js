d758ed5070fc32748c6fbf4cec823ca56617b0c1
Kim-Ha-Jeong <kha0318@naver.com>
2020-02-11 22:02:05 +0900

    수습과제 추가

--- client/src/component/result/Row.js
+++ client/src/component/result/Row.js
@@ -4,30 +4,32 @@ import axios from 'axios';
 class Row extends Component {
 
   constructor(props) {
-        super(props);
-        this.delete = this.delete.bind(this);
-    }
-    delete() {
-        axios.get('/apiServer/notice/delete/'+this.props.obj._id)
-            .then(console.log('Deleted'))
-            .catch(err => console.log(err))
-    }
+    super(props);
+    this.delete = this.delete.bind(this);
+  }
+
+  delete() {
+    axios.get('/apiServer/notice/delete/' + this.props.obj._id)
+      .then(console.log('Deleted'))
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
-        <tr>
-          <td>
-            {this.props.obj.name}
-          </td>
-          <td>
-            {this.props.obj.student_id}
-          </td>
-          <td>
-            {this.props.obj.team}
-          </td>
-          <td>
-            {this.props.obj.pass}
-          </td>
-        </tr>
+      <tr>
+        <td>
+          {this.props.obj.name}
+        </td>
+        <td>
+          {this.props.obj.student_id}
+        </td>
+        <td>
+          {this.props.obj.team}
+        </td>
+        <td>
+          {this.props.obj.pass}
+        </td>
+      </tr>
     );
   }
 }
