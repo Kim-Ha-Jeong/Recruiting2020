6e2d984005f8e554d7a5d8c2b3d2d840b42541be
root <root@ip-172-31-47-235.ap-northeast-2.compute.internal>
2020-02-11 13:31:54 +0000

    고치기 에러

--- client/src/component/result/Result.js
+++ client/src/component/result/Result.js
@@ -25,6 +25,11 @@ export default class Pass extends Component {
 
     var showDesign = document.getElementById('probation-design');
     var showContent = document.getElementById('probation-content');
+    
+    this.state.result.map(function(object,i){
+      console.log(object.team)
+      console.log(object.pass)
+    }
 
     this.state.result.map(function(object,i){
       if(object.team === "디자인팀" && object.pass === "합격"){
