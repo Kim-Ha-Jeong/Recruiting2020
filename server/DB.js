b8dba5be51d34d9787e871e5919d9b57f96fbbe9
root <root@ip-172-31-39-20.ap-northeast-2.compute.internal>
2020-02-20 16:59:16 +0000

    데이터베이스 환경설정

--- server/DB.js
+++ server/DB.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/recruiting2020'
+	DB: 'mongodb://kha0318:rlagkwjd0318@127.0.0.1:8080/recruiting2020?authSource=admin&authMechanism=SCRAM-SHA-1'
 }
